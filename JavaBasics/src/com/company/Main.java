40d794d228d6394fccd91d7fb66068a2ab44754f
krasiivanov <krasIvanov91@gmail.com>
2022-04-18 12:58:45 +0300

    New project

--- JavaBasics/src/com/company/Main.java
+++ JavaBasics/src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
+    	
+    	Scanner scanner = new Scanner(System.in);
+    	
+    	int b, s, d, ch;
+    	b = scanner.nextInt();
+    	s = scanner.nextInt();
+    	d = scanner.nextInt();
+    	
+    	ch = (b/s)/d;
+    	System.out.print("ch= " + ch);
+    	
+    	scanner.close();
     }
 }
